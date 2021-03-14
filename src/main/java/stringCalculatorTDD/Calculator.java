4465ad2f23d98997359c7db56e85417ccdbabdd3
HeroicHitesh <email.hiteshkumar@gmail.com>
2021-03-14 10:19:32 +0530

    Refactor String Splitter code

--- src/main/java/stringCalculatorTDD/Calculator.java
+++ src/main/java/stringCalculatorTDD/Calculator.java
@@ -4,17 +4,20 @@ public class Calculator {
 	public static int add(String numbers) {
 		if(numbers.isEmpty()) { 
 			return 0;
-		} else if(numbers.contains(",")){
-			String[] num=numbers.split(",");
+		} else {
+			String[] num = splitter(numbers);
 			int sum=0;
 			int size=num.length;
 			for(int i=0; i<size; i++){
 				sum = sum + toInt(num[i]);
 			}
 			return sum;
-		} else {
-			return toInt(numbers);
 		}
+	}
+
+	private static String[] splitter(String numbers) {
+		String[] num=numbers.split(",");
+		return num;
 	}
 
 	private static int toInt(String numbers) {
