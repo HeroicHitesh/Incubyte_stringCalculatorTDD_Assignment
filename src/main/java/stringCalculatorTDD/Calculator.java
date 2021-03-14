87d4f10c6bbae338b9c515781b1e00f857bbb0d9
HeroicHitesh <email.hiteshkumar@gmail.com>
2021-03-14 08:56:06 +0530

    Code to pass third test

--- src/main/java/stringCalculatorTDD/Calculator.java
+++ src/main/java/stringCalculatorTDD/Calculator.java
@@ -4,6 +4,9 @@ public class Calculator {
 	public static int add(String numbers) {
 		if(numbers.isEmpty()) { 
 			return 0;
+		} else if(numbers.contains(",")){
+			String[] num=numbers.split(",");
+			return Integer.parseInt(num[0])+Integer.parseInt(num[1]);
 		} else {
 			return Integer.parseInt(numbers);
 		}
